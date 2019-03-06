ab3c85ac0c9908487e1fee7e97c8d1053e2a66c4
Massimiliano Arione <garakkio@gmail.com>
2019-03-06 18:37:53 +0100

    avoid deprecation for Symfony 4.2

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -5,20 +5,12 @@ namespace Beelab\TestBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-/**
- * This is the class that validates and merges configuration from your app/config files.
- *
- * To learn more see {@link https://symfony.com/doc/current/bundles/extension.html}
- */
 class Configuration implements ConfigurationInterface
 {
-    /**
-     * {@inheritdoc}
-     */
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('beelab_test');
+        $treeBuilder = new TreeBuilder('beelab_test');
+        $rootNode = \method_exists($treeBuilder, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('beelab_test');
 
         $rootNode
             ->children()
