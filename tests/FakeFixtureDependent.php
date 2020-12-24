06ee2a1858449381a626e461861e713840ac69fe
Massimiliano Arione <garakkio@gmail.com>
2020-12-24 10:10:48 +0100

    💚 try to fix build (last try!)

--- tests/FakeFixtureDependent.php
+++ tests/FakeFixtureDependent.php
@@ -4,18 +4,36 @@ namespace Beelab\TestBundle\Tests;
 
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager as LegacyObjectManager;
 use Doctrine\Persistence\ObjectManager;
 
-final class FakeFixtureDependent implements DependentFixtureInterface, FixtureInterface
-{
-    public function getDependencies(): array
+if (\interface_exists(LegacyObjectManager::class)) {
+    final class FakeFixtureDependent implements DependentFixtureInterface, FixtureInterface
     {
-        return [
-            FakeFixture::class,
-        ];
-    }
+        public function getDependencies(): array
+        {
+            return [
+                FakeFixture::class,
+            ];
+        }
 
-    public function load(ObjectManager $manager): void
+        // do not add void return type here! For compatibility...
+        public function load(LegacyObjectManager $manager)
+        {
+        }
+    }
+} else {
+    final class FakeFixtureDependent implements DependentFixtureInterface, FixtureInterface
     {
+        public function getDependencies(): array
+        {
+            return [
+                FakeFixture::class,
+            ];
+        }
+
+        public function load(ObjectManager $manager): void
+        {
+        }
     }
 }
